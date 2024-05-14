d84583afd51c7ed5decaf67d9a74ee6c958ef0fd
RuanKysa <ruancjbueno@gmail.com>
2024-05-13 22:14:22 -0300

    aula-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './estilo.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import biscoito from './assets/biscoito.png';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      textoFrase: ''
+    };
+    this.quebrarBiscoito = this.quebrarBiscoito.bind(this);
+
+    this.frase = [
+     'Siga os bons e aprenda com eles',
+     'O bom-senso vale mais que muito conhecimento',
+     'O riso é a menor distãncia entre duas pessoas', 
+     'Deixe de lado as preocupações e seja feliz',
+     'Realize o óbvio, pense no improvável e conquiste o impossivel',
+     'Acredite em milagres mas não dependa deles', 
+     'A maior barreira para o sucesso é o medo do fracasso'];
+  }
+  quebrarBiscoito() {
+    let state = this.state;
+    let numeroAleatorio = Math.floor(Math.random() * this.frase.length)
+    state.textoFrase = this.frases[numeroAleatorio]
+    this.setState(state);
+  }
 }
-
-export default App;
